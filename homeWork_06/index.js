c60b135a53b19628ee4e500b5b7923f47d52c36a
Evgeny Tupitsyn <e_v_t_95@mail.ru>
2023-06-15 14:37:44 +0400

    add file index.js

---
+++ homeWork_06/index.js
@@ -0,0 +1,111 @@
+//////////////1//////////////////////////
+
+console.log('1');
+
+setTimeout(() => console.log('2'), 1);
+
+let promiseNew = new Promise((resolve) => {
+   console.log('3');
+   resolve();
+});
+
+promiseNew.then(() => console.log('4'));
+
+setTimeout(() => console.log('5'));
+
+console.log('6');
+
+Ответ: 1, 3, 6, 4, 5, 2
+
+//////////////////2////////////////////
+
+let promiseTree = new Promise((resolve, reject) => {
+resolve("a");
+
+console.log("1");
+
+setTimeout(() => {
+   console.log("2");
+}, 0);
+
+console.log("3");
+});
+
+Ответ: 1, 3, undefined, 2
+
+////////////////////3///////////////////
+
+let promiseTwo = new Promise((resolve, reject) => {
+   resolve("a");
+});
+
+promiseTwo
+.then((res) => {
+   return res + "b";
+})
+.then((res) => {
+   return res + "с";
+})
+.finally((res) => {
+   return res + "!!!!!!!";
+})
+.catch((res) => {
+   return res + "d";
+})
+.then((res) => {
+   console.log(res);
+});
+
+Ответ: abc
+
+///////////////4///////////////////
+
+function doSmth() {
+   return Promise.resolve("123");
+}
+
+doSmth()
+.then(function (a) {
+   console.log("", a); 
+   return a;
+})
+.then(function (b) {
+   console.log("", b);
+   return Promise.reject("321");
+})
+.catch(function (err) {
+   console.log("", err);
+})
+.then(function (c) {
+   console.log("", c);
+return c;
+});
+
+Ответ: 1 123, 2 123, 3 321, 4 undefined
+
+///////////////////5/////////////////
+
+console.log("1");
+
+setTimeout(function () {
+     console.log("2");
+}, 0);
+
+Promise.resolve().then(() => console.log("3"));
+
+console.log("4");
+
+Ответ: 1, 4, 3, undefined, 2
+
+
+
+////////////////////////6///////////////////////////////
+
+let arr = [10, 12, 15, 21]
+const showIndex = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 3000); }
+}
+showIndex(arr)
